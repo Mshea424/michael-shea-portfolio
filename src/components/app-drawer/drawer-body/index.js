d05e1196d85a01a2e3c4b3e68d0425de57a195a7
Mike Shea <mshea424@yahoo.com>
2021-12-30 15:18:08 -0500

    init app drawer body

---
+++ src/components/app-drawer/drawer-body/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./styles.css";
+
+export default function DrawerBody() {
+    return (
+        <div className='drawer-body'>
+        </div>
+    )
+}
--- src/components/app-drawer/index.js
+++ src/components/app-drawer/index.js
@@ -1,11 +1,13 @@
-import React from 'react'
-import HamburgerButton from './hamburger-button/index'
+import React from 'react';
+import HamburgerButton from './hamburger-button/index';
+import DrawerBody from './drawer-body/index';
 import "./styles.css";
 
 export default function AppDrawer() {
     return (
         <div>
             <HamburgerButton/>
+            <DrawerBody/>
         </div>
     )
 }
