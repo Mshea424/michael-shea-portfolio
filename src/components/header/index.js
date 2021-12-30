fdb8ae402668967e7e137d43fe378cc8c9e5e8e3
Mike Shea <mshea424@yahoo.com>
2021-12-30 10:19:07 -0500

    init Header

---
+++ src/components/header/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './styles.css'
+
+export default function Header() {
+    return (
+        <div className="header-wrapper">
+            <div className="header-text text-active">
+                Hi
+            </div>
+            <div  className="header-text">
+                I'm Mike
+            </div>
+            <div  className="header-text">
+                Welcome to my Portfolio
+            </div>
+        </div>
+    )
+}
