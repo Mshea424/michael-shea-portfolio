b6ed345c5d16e0bfb4b7a526cdc80917e8f9e0b4
Mike Shea <mshea424@yahoo.com>
2021-12-30 16:19:41 -0500

    updated styles & structure of app drawer opening animations

--- src/components/app-drawer/hamburger-button/index.js
+++ src/components/app-drawer/hamburger-button/index.js
@@ -1,25 +1,33 @@
-import React, {useState} from 'react'
+import React from 'react'
 import "./styles.css";
 
-export default function HamburgerButton() {
-    const [buttonActive, setButtonActive] = useState(false)
+export default function HamburgerButton(props) {
     return (
-        <div 
-            className='hamburger-button'
-            onClick={()=> setButtonActive(!buttonActive)}
-        >
+        <>                                      
             <div 
-                className={`hamburger-line ${buttonActive ? "active" : ""}`}
-                id="line1"
-            ></div>
+                className='hamburger-button'
+                onClick={()=> props.toggleDrawerActive()}
+            >
+                <div 
+                    className={`hamburger-line ${props.drawerActive ? "active" : ""}`}
+                    id="line1"
+                ></div>
+                <div 
+                    className={`hamburger-line ${props.drawerActive ? "active" : ""}`}
+                    id="line2"
+                ></div>
+                <div 
+                    className={`hamburger-line ${props.drawerActive ? "active" : ""}`}
+                    id="line3"
+                ></div>
+            </div>
             <div 
-                className={`hamburger-line ${buttonActive ? "active" : ""}`}
-                id="line2"
-            ></div>
-            <div 
-                className={`hamburger-line ${buttonActive ? "active" : ""}`}
-                id="line3"
-            ></div>
-        </div>
+                className={`button-shadow ${props.drawerActive ? "shadow-expand" : ""}`}
+                onClick={()=> props.toggleDrawerActive()}
+            >
+            
+            </div>
+        </>
+        
     )
 }
--- src/components/app-drawer/index.js
+++ src/components/app-drawer/index.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import HamburgerButton from './hamburger-button/index';
 import DrawerBody from './drawer-body/index';
 import "./styles.css";
 
 export default function AppDrawer() {
+    const [ drawerActive, setdrawerActive ] = useState(false);
+    const toggleDrawerActive = () => setdrawerActive(!drawerActive);
     return (
         <div>
-            <HamburgerButton/>
-            <DrawerBody/>
+            <div>{drawerActive ? 1 : 0}</div>
+            <button onClick={()=> setdrawerActive(!drawerActive)}>toggle state</button>
+            <HamburgerButton 
+                drawerActive={drawerActive}
+                toggleDrawerActive={toggleDrawerActive}
+            />
+            {/* <DrawerBody drawerActive={drawerActive}/> */}
         </div>
     )
 }
