0db26749d5a13dbbff9802ff7ecc707659e36960
Mike Shea <mshea424@yahoo.com>
2021-12-30 10:37:25 -0500

    added animations to header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,16 +1,28 @@
-import React from 'react'
+import { useState } from 'react'
 import './styles.css'
 
 export default function Header() {
+
+    const [ textCount, setTextCount ] = useState(1)
+
     return (
         <div className="header-wrapper">
-            <div className="header-text text-active">
+            <div 
+                className={`header-text ${textCount === 1 ? 'text-active' : ''}`}
+                onAnimationEnd={()=>{setTextCount(textCount + 1)}}
+            >
                 Hi
             </div>
-            <div  className="header-text">
+            <div 
+                className={`header-text ${textCount === 2 ? 'text-active' : ''}`}
+                onAnimationEnd={()=>{setTextCount(textCount + 1)}}
+            >
                 I'm Mike
             </div>
-            <div  className="header-text">
+            <div
+                className={`header-text ${textCount === 3 ? 'text-active' : ''}`}
+                onAnimationEnd={()=>{setTextCount(textCount + 1)}}
+            >
                 Welcome to my Portfolio
             </div>
         </div>
