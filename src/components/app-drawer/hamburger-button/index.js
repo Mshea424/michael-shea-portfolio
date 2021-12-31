d87b5c39ed7c93df2db96f579352a15d37cabee1
Mike Shea <mshea424@yahoo.com>
2021-12-31 16:22:55 -0500

    added closing animations for app drawer button

--- src/components/app-drawer/hamburger-button/index.js
+++ src/components/app-drawer/hamburger-button/index.js
@@ -9,20 +9,20 @@ export default function HamburgerButton(props) {
                 onClick={()=> props.toggleDrawerActive()}
             >
                 <div 
-                    className={`hamburger-line ${props.drawerActive ? "active" : ""}`}
+                    className={`hamburger-line ${props.drawerActive ? "active line-activate1" : "line-deactivate1"}`}
                     id="line1"
                 ></div>
                 <div 
-                    className={`hamburger-line ${props.drawerActive ? "active" : ""}`}
+                    className={`hamburger-line ${props.drawerActive ? "active line-activate2" : "line-deactivate2"}`}
                     id="line2"
                 ></div>
                 <div 
-                    className={`hamburger-line ${props.drawerActive ? "active" : ""}`}
+                    className={`hamburger-line ${props.drawerActive ? "active line-activate3" : "line-deactivate3"}`}
                     id="line3"
                 ></div>
             </div>
             <div 
-                className={`button-shadow ${props.drawerActive ? "shadow-expand" : ""}`}
+                className={`button-shadow ${props.drawerActive ? "active shadow-activate" : "shadow-deactivate"}`}
                 onClick={()=> props.toggleDrawerActive()}
             >
             
