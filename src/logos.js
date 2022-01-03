0fd293f660072d61afbd9ca6911c20ef17f43702
Mike Shea <mshea424@yahoo.com>
2022-01-03 15:30:29 -0500

    update url refs

--- src/logos.js
+++ src/logos.js
@@ -1,9 +1,12 @@
 const logos = {
     itemsType:'logo',
-    logoUrls: [ 'images/css_logo.png',
-        './images/express_logo.png',
+    logoUrls: [ './images/html_logo.png',
+        'images/css_logo.png',
+        'images/js_logo.png',
+        'images/react_logo.png',
+        './images/node_express_logo.png',
         './images/git_github_logo.png',
-        './images/html_logo.png'
+        
     ]
 };
 
