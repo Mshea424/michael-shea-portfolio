482be985ebf95b9475a92c3fa89752ee03455ea7
Mike Shea <mshea424@yahoo.com>
2022-01-06 01:50:01 -0500

    close drawer on nav

--- src/components/app-drawer/index.js
+++ src/components/app-drawer/index.js
@@ -12,7 +12,10 @@ export default function AppDrawer() {
                 drawerActive={drawerActive}
                 toggleDrawerActive={toggleDrawerActive}
             />
-            <DrawerBody drawerActive={drawerActive}/>
+            <DrawerBody 
+                drawerActive={drawerActive}
+                toggleDrawerActive={toggleDrawerActive}
+            />
         </div>
     )
 }
