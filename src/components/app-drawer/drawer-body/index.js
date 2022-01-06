504d24899ccf0267f0647289354e0982bf341a1d
Mike Shea <mshea424@yahoo.com>
2022-01-06 01:50:26 -0500

    update drawer for mobile view

--- src/components/app-drawer/drawer-body/index.js
+++ src/components/app-drawer/drawer-body/index.js
@@ -5,10 +5,34 @@ export default function DrawerBody(props) {
         <div 
             className={`drawer-body ${props.drawerActive ? "active drawer-activate" : "drawer-deactivate"}`}
         >
-            <a href="#about" className='drawer-nav'>About Me</a>
-            <a href="#skills" className='drawer-nav'>Technologies</a>
-            <a href="#projects" className='drawer-nav'>Projects</a>
-            <a href="#contact" className='drawer-nav'>Contact</a>
+            <a 
+                href="#about" 
+                className='drawer-nav'
+                onClick={()=> props.toggleDrawerActive()}
+            >
+                About Me
+            </a>
+            <a 
+                href="#skills" 
+                className='drawer-nav'
+                onClick={()=> props.toggleDrawerActive()}
+            >
+                Technologies
+            </a>
+            <a 
+                href="#projects" 
+                className='drawer-nav'
+                onClick={()=> props.toggleDrawerActive()}
+            >
+                Projects
+            </a>
+            <a 
+                href="#contact" 
+                className='drawer-nav'
+                onClick={()=> props.toggleDrawerActive()}
+            >
+                Contact
+            </a>
         </div>
     )
 }
