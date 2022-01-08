28b115fcc585d0a31afa02fd4742f52692543088
Mike Shea <mshea424@yahoo.com>
2022-01-08 16:56:20 -0500

    init project component

--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -1,10 +1,37 @@
+import './styles.css'
 import '../../section.css';
 import '../../grid.css';
 
 export default function Projects() {
     return (
         <div className="section">
-            <div className="section-title" id="projects">My Projects</div> 
+            <div className="section-title" id="projects">My Projects</div>
+            <div className="grid-wrapper">
+                <a 
+                    href="https://www.qbcart.net/" 
+                    className="project-wrapper"
+                    target="_blank" 
+                    rel="noopener noreferrer"
+                >
+                    <div 
+                        className="project-img"
+                        style={ { backgroundImage: 'url(images/qbcart_logo.jpeg)'} }
+                    >
+                    
+                    </div>
+                    <div className="project-desc">
+                    QBCart
+                    </div>
+                </a>
+                <div className="project-wrapper">
+                    <div className="project-img">
+                    
+                    </div>
+                    <div className="project-desc">
+                    Americonnect
+                    </div>
+                </div>
+            </div> 
         </div>
     )
 }
