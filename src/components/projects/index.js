6dcef1b9500ea480e28d30fb95f2f6d78c9c3672
Mike Shea <mshea424@yahoo.com>
2022-01-10 17:55:21 -0500

    init project titles

--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -20,7 +20,10 @@ export default function Projects() {
                     
                     </div>
                     <div className="project-desc">
-                    QBCart
+                        <div className="project-title">QBCart</div>
+                        <div className=" project-subtitle truncate-overflow">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. 
+                        </div> 
                     </div>
                 </a>
                 <div className="project-wrapper reversed">
@@ -28,7 +31,10 @@ export default function Projects() {
                     
                     </div>
                     <div className="project-desc">
-                    Americonnect
+                    <div className="project-title">Americonnect</div>
+                        <div className=" project-subtitle truncate-overflow">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </div>
                     </div>
                 </div>
             </div> 
