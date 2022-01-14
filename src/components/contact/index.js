4bc2eb4f55f802a7ee56a04d283cc1caf1b58be0
Mike Shea <mshea424@yahoo.com>
2022-01-14 14:47:59 -0500

    init contact section

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import './styles.css'
 
 export default function Contact() {
     return (
         <div className="section">
             <div className="section-title" id="contact">Contact Me</div>
+            <a 
+                className="contact-link"
+                href="https://www.linkedin.com/in/michael-s-shea/"
+                target="_blank" 
+                rel="noopener noreferrer"
+                style={ { backgroundImage: 'url(images/linkedin_link.png)'} }
+            ></a>
+            <a 
+                className="contact-link"
+                href="https://github.com/mshea424"
+                target="_blank" 
+                rel="noopener noreferrer"
+                style={ { backgroundImage: 'url(images/github_link.png)'} }
+            ></a>
         </div>
     )
 }
