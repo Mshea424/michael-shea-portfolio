395aa4dbb3d959c977e7b22c3b5c6800058d433b
Mike Shea <mshea424@yahoo.com>
2022-01-14 00:49:34 -0500

    update project desc

--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -22,7 +22,8 @@ export default function Projects() {
                     <div className="project-desc">
                         <div className="project-title">QBCart</div>
                         <div className=" project-subtitle truncate-overflow">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. 
+                         QBCart is a business software suite that extends Quick Books with a variety of additional features. 
+                         My role was to build the front-end web experience to allow QBCArt customers to easily launch Ecommerce sites and integrate them directly with QuickBooks.
                         </div> 
                     </div>
                 </a>
@@ -33,7 +34,32 @@ export default function Projects() {
                     <div className="project-desc">
                     <div className="project-title">Americonnect</div>
                         <div className=" project-subtitle truncate-overflow">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            Ammericonnect is a Trucking company based in Canada. 
+                            I built a new modern responsive website for Americonnect, including a quote tool that allowed them to drive business from internet traffic. As of this writing, the company has not yet taken the site live.
+                        </div>
+                    </div>
+                </div>
+                <div className="project-wrapper">
+                    <div className="project-img">
+                    
+                    </div>
+                    <div className="project-desc">
+                    <div className="project-title">Clarity</div>
+                        <div className=" project-subtitle truncate-overflow">
+                            Clarity is an application built in Angular which allows a commercial insurance broker to produce attractive visualizations of complex corporate insurance strategies. 
+                            I was primarily responsible for the UI.
+                        </div>
+                    </div>
+                </div>
+                <div className="project-wrapper reversed">
+                    <div className="project-img">
+                    
+                    </div>
+                    <div className="project-desc">
+                    <div className="project-title">Sunset Yards</div>
+                        <div className=" project-subtitle truncate-overflow">
+                            Sunset Yards is a High-end Commercial Real estate project in New York City. 
+                            I built Sunset Yard's new website, along with another developer, coded completely in Vanilla JS, CSS, and HTML.
                         </div>
                     </div>
                 </div>
