5d6b5f609b83fb63f89b788916b186ab88f2894d
Mike Shea <mshea424@yahoo.com>
2022-01-13 21:20:32 -0500

    updated text

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -8,11 +8,9 @@ export default function About() {
                 About Me
             </div>
             <div className="section-subtitle">
-            I'm a software engineer, Atlanta resident, and recent graduate of General Assembly. 
+            I'm a software engineer, Atlanta resident, and graduate of General Assembly. 
 
-I've been passionate about computers since a young age, and in 2020 I dedicated myself full-time to a career in tech.
-
-My primary goals as a software engineer are to grow my skills, to build technologies that solve real problems in efficient ways, and one day provide easy ways for others to learn and use technology.
+I've been passionate about computers since a young age, and in 2020 I dedicated myself full-time to a career in tech. I focus primarily on building full-stack applications using React and Node.js.
             </div>
         </div>
     )
