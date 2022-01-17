578ec6e50a91b61288ffa5c5c1689afccc029741
Mike Shea <mshea424@yahoo.com>
2022-01-17 16:12:52 -0500

    update logos

--- src/components/skills/logoUrls.js
+++ src/components/skills/logoUrls.js
@@ -1,14 +1,32 @@
-const logoUrls = [ 'images/html_logo.png',
+const logoUrls = [ 
+        'images/html_logo.png',
         'images/css_logo.png',
         'images/js_logo.png',
+        'images/ts_logo.png',
+
         'images/react_logo.png',
+        'images/angular_logo.png',
         'images/node_express_logo.png',
-        'images/git_github_logo.png',  
+        'images/lit_logo.png',
+
         'images/mongo_logo.png',
         'images/postgresql_logo.png',
+        'images/azure_functions_logo.png',
+        'images/cosmos_logo.png',
+       
+        'images/styled_components_logo.png',
+        'images/material_logo.png',
+        'images/bootstrap_logo.png',  
+        
+        
         'images/vscode_logo.png',
+        'images/git_github_logo.png',
+
         'images/netlify_logo.png',
-        'images/heroku_logo.png'
+        'images/heroku_logo.png',
+        
+        'images/webpack_logo.png',
+        'images/snowpack_logo.png'
     ];
 
 export default logoUrls
